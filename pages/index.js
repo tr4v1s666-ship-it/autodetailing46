26378fad8b11ee417385f4cf9469f88828c71990
tr4v1s666-ship-it <tr4v1s666@gmail.com>
2026-05-22 15:05:15 +0200

    Add home page with WhatsApp booking button

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    <div style={{
+      background: "black",
+      color: "white",
+      minHeight: "100vh",
+      textAlign: "center",
+      paddingTop: "100px"
+    }}>
+      <h1>AutoDetailing46</h1>
+      <p>Detailing automobile premium à Saint-Céré</p>
+
+      <a href="https://wa.me/33662550681">
+        <button style={{ padding: "15px", marginTop: "20px" }}>
+          Réserver sur WhatsApp
+        </button>
+      </a>
+    </div>
+  );
+}
